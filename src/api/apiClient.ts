83a1cb01e336b337c174be2fa3a1725b884ceab3
JonnathanI <129433201+JonnathanI@users.noreply.github.com>
2025-03-12 15:00:06 -0500

    Update apiClient.ts

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8081', // URL de tu backend
+  baseURL: 'https://certificados.sudamericano.edu.ec/api', // URL de tu backend
   headers: {
     'Content-Type': 'application/json',
   },
